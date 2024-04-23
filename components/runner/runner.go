a199944a3b327513e52a9aa29f1a20d95b6711e1
Lucas Hideki Carvalho Dinnouti <ldinnouti@gmail.com>
2024-04-22 22:18:52 -0300

    Read WhatsApp backup file

--- components/runner/runner.go
+++ components/runner/runner.go
@@ -1,35 +1,96 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
+	"strings"
 	"sync"
-	"time"
 )
 
-var hits map[string]int
 var mutex = &sync.Mutex{}
+var testcase []*Message
 
-func request() {
-	mutex.Lock()
-	hits["instance"]++
+type Message struct {
+	datetime string
+    content string
+}
 
+func parseLine(line string) (*Message, error) {
+
+	match, err := regexp.MatchString(`\d+\/\d+\/\d+\, \d{2}\:\d{2} \- .*`, line)
+
+    if (err != nil || !match) {
+        return nil, errors.New("Invalid Line")
+    }
+
+    sep := " - "
+	s := strings.Split(line, sep)
+    datetime, content := s[0], s[1]
+
+    message := Message{
+        datetime: datetime,
+        content: content,
+    }
+
+	return &message, nil
+}
+
+func load_testcase(filename string) {
+
+	file, err := os.Open(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	fileScanner := bufio.NewScanner(file)
+	line := ""
+ 
+    fileScanner.Split(bufio.ScanLines)
+  
+    for fileScanner.Scan() {
+		line = fileScanner.Text()
+		
+		message, err := parseLine(line)
+		if (err == nil) {
+			testcase = append(testcase, message)
+			log.Println(message)
+		}
+    }
+
+    defer file.Close()
+
+}
+
+func run_testcase() {
+	for _, message := range testcase {
+		request(message)
+	}
+}
+
+func request(message *Message) {
+	body, err := json.Marshal(message)
 	log.Println("Requesting processor...")
-	_, err := http.Get("http://proxy.default.svc.cluster.local:8082/test")
+	log.Println(body)
+
+	result, err := http.Post(
+		"http://proxy.default.svc.cluster.local:8082/test",
+		"application/json",
+		bytes.NewBuffer(body))
+
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	mutex.Unlock()
+    log.Println(result)
 }
 
 func main() {
-	hits = map[string]int{
-		"instance": 0,
-	}
-
-	for {
-		request()
-		time.Sleep(5 * time.Second)
-	}
+	load_testcase("../../datasets/chat_dos/chat_dos.txt")
+	run_testcase()
 }
