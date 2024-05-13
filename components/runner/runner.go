700c62c6b867a4baeb7fdba7ab980431b7f49ac5
Lucas Hideki Carvalho Dinnouti <ldinnouti@gmail.com>
2024-05-13 20:26:35 -0300

    Fix runner metrics

--- components/runner/runner.go
+++ components/runner/runner.go
@@ -26,7 +26,7 @@ var (
 	requestDurations = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name: "full_request_latency",
 		Help: "Latency of requests to processor",
-		Buckets: []float64{0.001, 0.005, 0.010, 0.100, 0.500, 1, 5, 10}})
+		Buckets: []float64{0.001, 0.005, 0.010, 0.100, 0.500, 1, 2, 3, 4, 5, 6, 8, 9, 10}})
 )
 
 type ContentType uint8
@@ -158,11 +158,10 @@ func main() {
 
 	http.Handle("/metrics", promhttp.Handler())
 
+	go http.ListenAndServe(":8081", nil)
 
 	log.Println("Starting Runner!")
 
 	LoadTestCase("testcase_1.txt")
 	RunTestCase()
-
-	http.ListenAndServe(":8081", nil)
 }
