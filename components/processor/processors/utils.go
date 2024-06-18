9ca7d5b32ca742474ebde3b1e07199757afed6ec
julia.arruda <julia.arruda@julia.arruda>
2024-06-17 21:10:20 -0300

    Fix http proxy error: EOF

--- components/processor/processors/utils.go
+++ components/processor/processors/utils.go
@@ -1,40 +1,40 @@
 package processors
 
-import(
+import (
 	"math/rand"
-	"strings"
 	"strconv"
+	"strings"
 )
-
 
 func WasteTime(a int, b int) {
 
-	var isPrimeSlice = []string{ "Is Prime.", "Is Prime." }
+	var isPrimeSlice = []string{"Is Prime.", "Is Prime."}
 	isPrimeSlice = nil
+	primes := 0
 
 	baseMultiplier := 45
 	memMultiplier := 1
 
-	n := (a + rand.Intn(b - a)) * baseMultiplier
+	n := (a + rand.Intn(b-a)) * baseMultiplier
 
 	for i := 2; i < n; i++ {
-        if isPrime(i) {
-			isPrimeSlice = append(isPrimeSlice, strings.Repeat(strconv.Itoa(i) + " is Prime.", memMultiplier))
+		if isPrime(i) {
+			isPrimeSlice = append(isPrimeSlice, strings.Repeat(strconv.Itoa(i)+" is Prime.", memMultiplier))
+			primes++
 		}
-    }
+	}
 
-    println(isPrimeSlice[rand.Intn(n - 1)])
+	println(isPrimeSlice[rand.Intn(primes-1)])
 }
 
 func isPrime(n int) bool {
-    if n <= 1 {
-        return false
-    }
-    for i := 2; i < n; i++ {
-        if n % i == 0 {
-            return false
-        }
-    }
-    return true
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
-
--- scripts/local-infra-up.sh
+++ scripts/local-infra-up.sh
@@ -6,8 +6,8 @@ kind load docker-image processor:latest --name crp
 kind load docker-image runner:latest --name crp
 kind load docker-image proxy:latest --name crp
 
+kubectl apply -f ./infra/manifests/metrics
+
 export ALGORITHM="round_robin"
 
 for f in ./infra/manifests/components/*.yaml; do envsubst < $f | kubectl apply -f -; done
-
-kubectl apply -f ./infra/manifests/metrics
